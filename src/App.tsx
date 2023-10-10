9852e9e6d5376535711f24a85f1caa537cfc9451
sw4pn <sw4pn@yahoo.com>
2023-10-10 16:25:14 +0530

    added options

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
-import { isMobile } from "react-device-detect";
+import {
+  isMobile,
+  isTablet,
+  deviceDetect,
+  deviceType,
+} from "react-device-detect";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    if (window) {
+      setInfo(deviceDetect);
+    }
+  }, []);
+  console.log("device details: ", info);
+
   return (
     <div className="hero">
-      {isMobile ? "This is a mobile device" : "This is not a mobile device"}
+      {isMobile ? (
+        <>
+          This is a <b>mobile</b> device
+        </>
+      ) : (
+        <>
+          This is a <b> NOT</b> mobile device
+        </>
+      )}
+      <br />
+      <hr color="#efefef" />
+      <br />
+      {isTablet ? (
+        <>
+          This is a <b>tablet</b> device
+        </>
+      ) : (
+        <>
+          This is a <b>NOT</b> tablet device
+        </>
+      )}
+      <br />
+      <hr color="#efefef" />
+      <br />
+      device Type:{" "}
+      <span style={{ color: "green" }}> {deviceType?.toUpperCase()}</span>
+      <br />
+      <hr color="#efefef" />
+      <br />
+      <div className="info">
+        <pre>{JSON.stringify(info, null, 2)}</pre>
+      </div>
     </div>
   );
 }
